c3a00e22dc709b541b186ecba668187d854fce85
Robson Arruda <robsonshockwavegithub@gmail.com>
2024-02-18 22:19:28 -0300

    Consumindo uma stream completa

--- streams/stream-http-server.js
+++ streams/stream-http-server.js
@@ -14,8 +14,20 @@ class InverseNumberStream extends Transform {
 // req => ReadableStream
 // res => WritableStream
 
-const server = http.createServer((req, res) => {
-  return req.pipe(new InverseNumberStream()).pipe(res);
+const server = http.createServer(async (req, res) => {
+  const buffers = [];
+
+  for await (const chunk of req) {
+    buffers.push(chunk);
+  }
+
+  const fullStreamContent = Buffer.concat(buffers).toString();
+
+  console.log(fullStreamContent);
+
+  res.end(fullStreamContent);
+
+  //   return req.pipe(new InverseNumberStream()).pipe(res);
 });
 
 server.listen(3334);
