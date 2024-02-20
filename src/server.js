5ff3b6c8cdd190ef173853de2a295133a3e7441e
Robson Arruda <robsonshockwavegithub@gmail.com>
2024-02-19 21:44:04 -0300

    criando objeto como banco de dados

--- src/server.js
+++ src/server.js
@@ -1,5 +1,8 @@
 import http from 'node:http';
 import { json } from './middlewares/json.js';
+import { Database } from './database.js';
+
+const database = new Database();
 
 // - Criar usuários
 // - Listagem usuários
@@ -27,25 +30,27 @@ import { json } from './middlewares/json.js';
 
 // HTTP Status Code
 
-const users = [];
-
 const server = http.createServer(async (req, res) => {
   const { method, url } = req;
 
   await json(req, res);
 
   if (method === 'GET' && url === '/users') {
+    const users = database.select('users');
+
     return res.end(JSON.stringify(users));
   }
 
   if (method === 'POST' && url === '/users') {
     const { name, email } = req.body;
 
-    users.push({
+    const user = {
       id: 1,
       name,
       email,
-    });
+    };
+
+    database.insert('users', user);
 
     return res.writeHead(201).end();
   }
